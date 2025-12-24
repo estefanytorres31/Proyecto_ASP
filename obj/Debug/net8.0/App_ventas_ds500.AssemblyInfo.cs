0d97f8f0ba8a6f9cb0daf2b2e139647e21646196
Estefany Torres <estefanyt3101@gmail.com>
2025-12-24 08:59:25 -0500

    Update AssemblyInformationalVersion in AssemblyInfo.cs
    Changed the AssemblyInformationalVersion attribute to include a build metadata hash. This helps with identifying the specific build version.

--- obj/Debug/net8.0/App_ventas_ds500.AssemblyInfo.cs
+++ obj/Debug/net8.0/App_ventas_ds500.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App_ventas_ds500")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b962d3645347bde3e04ebee68527a02d5c275560")]
 [assembly: System.Reflection.AssemblyProductAttribute("App_ventas_ds500")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App_ventas_ds500")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
